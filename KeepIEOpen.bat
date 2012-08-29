32e98e2d2ed9797e59e2fe4d4dae800bf9c1ee2f
Garrett Heath Koller <garrettheath4@gmail.com>
2012-08-29 16:56:30 -0400

    Made InPrivate start with KioskHome.html page

--- KeepIEOpen.bat
+++ KeepIEOpen.bat
@@ -1,4 +1,4 @@
 :loop
-"C:\Program Files\Internet Explorer\iexplore.exe" -k -private
+"C:\Program Files\Internet Explorer\iexplore.exe" -k -private "file:///C:/IE-Kiosk/KioskHome.html"
 timeout 3
 GOTO loop
