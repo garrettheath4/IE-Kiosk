9b58b366f594401f656135130c7f07b6eaf2ce5f
Garrett Heath Koller <garrettheath4@gmail.com>
2012-08-31 14:24:55 -0400

    Generalized Update.bat to be username-independent

--- Update.bat
+++ Update.bat
@@ -1,4 +1,4 @@
-set gitdir="C:\Users\pubkio14\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7"
+set gitdir="C:\Users\%username%\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7"
 set path=%gitdir%\cmd;%path%
 
 cd C:\IE-Kiosk
