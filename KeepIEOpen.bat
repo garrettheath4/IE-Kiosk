7d4499f49756857bad1fa3a575588b260a23dd68
Garrett Heath Koller <garrettheath4@gmail.com>
2012-08-31 10:52:23 -0400

    Disabled kiosk mode

--- KeepIEOpen.bat
+++ KeepIEOpen.bat
@@ -1,4 +1,4 @@
 :loop
-"C:\Program Files\Internet Explorer\iexplore.exe" -k -private "file:///C:/IE-Kiosk/KioskHome.html"
+"C:\Program Files\Internet Explorer\iexplore.exe" -private "file:///C:/IE-Kiosk/KioskHome.html"
 timeout 5
 GOTO loop
