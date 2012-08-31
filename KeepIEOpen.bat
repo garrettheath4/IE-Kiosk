e676aefc493565730640853fb977cc9c7ba69098
Garrett Heath Koller <garrettheath4@gmail.com>
2012-08-31 10:59:58 -0400

    Decreased IE restart time and generalized to just iexplore.exe

--- KeepIEOpen.bat
+++ KeepIEOpen.bat
@@ -1,4 +1,4 @@
 :loop
-"C:\Program Files\Internet Explorer\iexplore.exe" -private "file:///C:/IE-Kiosk/KioskHome.html"
-timeout 5
+iexplore.exe -private "file:///C:/IE-Kiosk/KioskHome.html"
+timeout 2
 GOTO loop
