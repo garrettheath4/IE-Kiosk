9ee84025d47bc742ba209b1aea1f8bf49a62dcc7
Garrett Heath Koller <garrettheath4@gmail.com>
2013-08-22 11:57:56 -0400

    Added quotes around Git directory

--- Update.bat
+++ Update.bat
@@ -1,5 +1,5 @@
 REM set gitdir="C:\Users\%username%\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7"
-for /f "usebackq" %%m in (`dir /b C:\Users\%username%\AppData\Local\GitHub\PortableGit_*`) do (
+for /f "usebackq" %%m in (`dir /b "C:\Users\%username%\AppData\Local\GitHub\PortableGit_*"`) do (
 	set gitdir="C:\Users\%username%\AppData\Local\GitHub\%%m"
 )
 set path=%gitdir%\cmd;%path%
