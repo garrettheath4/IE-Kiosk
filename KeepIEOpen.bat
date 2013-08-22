46878454b9e858825a1718c7ab63f071ac759eb2
Garrett Heath Koller <garrettheath4@gmail.com>
2013-08-22 15:49:11 -0400

    Added 2 min timeout if IE already running

--- KeepIEOpen.bat
+++ KeepIEOpen.bat
@@ -2,6 +2,7 @@
 
 tasklist /nh /fi "imagename eq iexplore.exe" | find /i "iexplore.exe" >nul && (
 echo Internet Explorer is already running
+timeout 120
 ) || (
 echo Starting Internet Explorer kiosk mode...
 "C:\Program Files\Internet Explorer\iexplore.exe" -private "file:///C:/IE-Kiosk/KioskHome.html"
