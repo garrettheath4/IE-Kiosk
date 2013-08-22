19bde936aa14c64c84d6aa4feeeaac5085af21d5
Garrett Heath Koller <garrettheath4@gmail.com>
2013-08-22 16:27:56 -0400

    Added indentation to improve readability

--- KeepIEOpen.bat
+++ KeepIEOpen.bat
@@ -1,11 +1,10 @@
 :loop
 
 tasklist /nh /fi "imagename eq iexplore.exe" | find /i "iexplore.exe" >nul && (
-echo Internet Explorer is already running
-timeout 120
+	echo Internet Explorer is already running
 ) || (
-echo Starting Internet Explorer kiosk mode...
-"C:\Program Files\Internet Explorer\iexplore.exe" -private "file:///C:/IE-Kiosk/KioskHome.html"
+	echo Starting Internet Explorer kiosk mode...
+	"C:\Program Files\Internet Explorer\iexplore.exe" -private "file:///C:/IE-Kiosk/KioskHome.html"
 )
 
 timeout 5
